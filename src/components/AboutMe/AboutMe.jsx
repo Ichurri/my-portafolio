b0c66fb7065b4c9e4d58d14a7c0216bd82a96853
Ichurri <santiagoiturrivargas04@gmail.com>
2024-12-19 19:16:07 -0400

    changes in aboutme

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -5,13 +5,15 @@ export const AboutMe = () => {
     return (
         <section id="about-me">
             <div className="wrapper">
-                <header>
-                    <h2>About Me</h2>
-                </header>
+                <div className="about-me-header">
+                    <header>
+                        <h2>About Me</h2>
+                    </header>
+                </div>
+                <div className="profile-container">
+                        <img src='src/assets/images/profile.png' alt="Profile" className="profile-pic" />
+                </div>
                 <div className="content">
-                    <div className="profile-container">
-                        <img src="src/assets/images/profile.jpeg" alt="Profile" className="profile-pic" />
-                    </div>
                     <div className="text-container">
                         <p>
                             I'm Santiago Iturri, a passionate web developer, always eager to learn new skills and improve my craft.
