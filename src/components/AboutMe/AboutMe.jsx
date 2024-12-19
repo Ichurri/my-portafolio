08d7c8c758f92026567eed93bf48ef6897829a9a
Ichurri <santiagoiturrivargas04@gmail.com>
2024-12-19 16:22:28 -0400

    modify about me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './AboutMe.css';
 
 export const AboutMe = () => {
@@ -13,15 +14,18 @@ export const AboutMe = () => {
                     </div>
                     <div className="text-container">
                         <p>
-                            I am Santiago Iturri, a passionate web developer, I am always eager to learn new skills and improve my craft.
+                            I'm Santiago, a passionate web developer, I'm always eager to learn new skills and improve my craft.
                         </p>
                         <div className="details">
                             <ul>
                                 <li>System Engineer Student</li>
                                 <li>Passionate Programmer</li>
-                                <li>Contact: santiagoiturrivargas04@gmail.com</li>
                             </ul>
                         </div>
+                        <p className="mailto-link">
+                            {/* <Link to="mailto:santiagoiturrivargas04@gmail.com">Contact me via Email</Link> */}
+                            <a href="mailto:santiagoiturrivargas04@gmail.com">Contact me via Email</a>
+                        </p>
                     </div>
                 </div>
             </div>
