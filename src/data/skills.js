974c7ba482b50a38411490c8f54664a14b2b9440
Ichurri <santiagoiturrivargas04@gmail.com>
2024-12-19 16:55:50 -0400

    change images' routes

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,50 +1,50 @@
 export const skills = [
     {
-        logoSrcFront: '/assets/logos/cpp.png',
-        logoSrcBack: '/assets/logos/cpp.png',  
+        logoSrcFront: 'src/assets/logos/cpp.png',
+        logoSrcBack: 'src/assets/logos/cpp.png',  
         title: 'C++',
         description: 'Advanced knowledge of C++ for algorithms and data structures, object-oriented programming, and low-level system operations.'
     },
     {
-        logoSrcFront: '/assets/logos/python.png',
-        logoSrcBack: '/assets/logos/python.png',  
+        logoSrcFront: 'src/assets/logos/python.png',
+        logoSrcBack: 'src/assets/logos/python.png',  
         title: 'Python',
         description: 'Experienced with Python for data analysis, web development, and engineering applications.'
     },
     {
-        logoSrcFront: '/assets/logos/html.png',
-        logoSrcBack: '/assets/logos/css.png',
+        logoSrcFront: 'src/assets/logos/html.png',
+        logoSrcBack: 'src/assets/logos/css.png',
         title: 'HTML/CSS',
         description: 'Proficient in building responsive and accessible web layouts using semantic HTML and modern CSS techniques.'
     },
     {
-        logoSrcFront: '/assets/logos/react.png',
-        logoSrcBack: '/assets/logos/react.png',  
+        logoSrcFront: 'src/assets/logos/react.png',
+        logoSrcBack: 'src/assets/logos/react.png',  
         title: 'React JavaScript',
         description: 'Building dynamic single-page applications using React, managing state and component lifecycle efficiently.'
     },
     {
-        logoSrcFront: '/assets/logos/javascript.png',
-        logoSrcBack: '/assets/logos/typescript.png', 
+        logoSrcFront: 'src/assets/logos/javascript.png',
+        logoSrcBack: 'src/assets/logos/typescript.png', 
         title: 'JavaScript & TypeScript',
         description: 'Experienced with JavaScript and TypeScript, using OOP and asynchronous programming patterns.'
     },
     {
-        logoSrcFront: '/assets/logos/nodejs.png',
-        logoSrcBack: '/assets/logos/expressjs.png', 
+        logoSrcFront: 'src/assets/logos/nodejs.png',
+        logoSrcBack: 'src/assets/logos/expressjs.png', 
         title: 'Node & Express',
         description: 'Developing back-end services with Node.js and Express.js, focusing on API development.'
     },
     {
-        logoSrcFront: '/assets/logos/sql.png',
-        logoSrcBack: '/assets/logos/sql.png', 
+        logoSrcFront: 'src/assets/logos/sql.png',
+        logoSrcBack: 'src/assets/logos/sql.png', 
         title: 'SQL',
         description: 'Skilled in designing and querying relational databases using SQL for data management and analysis.'
     },
 
     {
-        logoSrcFront: '/assets/logos/mongodb.png',
-        logoSrcBack: '/assets/logos/mongodb.png', 
+        logoSrcFront: 'src/assets/logos/mongodb.png',
+        logoSrcBack: 'src/assets/logos/mongodb.png', 
         title: 'MongoDB',
         description: 'Proficient in using MongoDB as a NoSQL database for scalable, high-performance applications.'
     }
